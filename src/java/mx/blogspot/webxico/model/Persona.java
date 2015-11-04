a385b7bb0ee640a049736c6b2327ea952f5b36c9
WEBXICO-MX <weiss.uttab@gmail.com>
2015-11-04 14:42:32 -0600

    Atributo "sexo" se cambia a String. Se agrega atributo "foto" de tipo String. Se inicializan atributos en el constructor.

--- src/java/mx/blogspot/webxico/model/Persona.java
+++ src/java/mx/blogspot/webxico/model/Persona.java
@@ -8,28 +8,40 @@ package mx.blogspot.webxico.model;
 import java.io.Serializable;
 import java.util.Date;
 
-public class Persona implements Serializable{
+public class Persona implements Serializable {
 
     private int cve_persona;
     private String nombre;
     private String ap_paterno;
     private String ap_materno;
-    private boolean sexo;
+    private String sexo;
     private Date fecha_nacimiento;
+    private String foto;
     private Date fecha_reg;
     private Date fecha_mod;
     private boolean activo;
 
     public Persona() {
+        this.cve_persona = 0;
+        this.nombre = "";
+        this.ap_paterno = "";
+        this.ap_materno = "";
+        this.sexo = "";
+        this.fecha_nacimiento = null;
+        this.foto = "";
+        this.fecha_reg = null;
+        this.fecha_mod = null;
+        this.activo = false;
     }
 
-    public Persona(int cve_persona, String nombre, String ap_paterno, String ap_materno, boolean sexo, Date fecha_nacimiento, Date fecha_reg, Date fecha_mod, boolean activo) {
+    public Persona(int cve_persona, String nombre, String ap_paterno, String ap_materno, String sexo, Date fecha_nacimiento, String foto, Date fecha_reg, Date fecha_mod, boolean activo) {
         this.cve_persona = cve_persona;
         this.nombre = nombre;
         this.ap_paterno = ap_paterno;
         this.ap_materno = ap_materno;
         this.sexo = sexo;
         this.fecha_nacimiento = fecha_nacimiento;
+        this.foto = foto;
         this.fecha_reg = fecha_reg;
         this.fecha_mod = fecha_mod;
         this.activo = activo;
@@ -67,11 +79,11 @@ public class Persona implements Serializable{
         this.ap_materno = ap_materno;
     }
 
-    public boolean isSexo() {
+    public String getSexo() {
         return sexo;
     }
 
-    public void setSexo(boolean sexo) {
+    public void setSexo(String sexo) {
         this.sexo = sexo;
     }
 
@@ -81,6 +93,14 @@ public class Persona implements Serializable{
 
     public void setFecha_nacimiento(Date fecha_nacimiento) {
         this.fecha_nacimiento = fecha_nacimiento;
+    }
+
+    public String getFoto() {
+        return foto;
+    }
+
+    public void setFoto(String foto) {
+        this.foto = foto;
     }
 
     public Date getFecha_reg() {
@@ -109,7 +129,7 @@ public class Persona implements Serializable{
 
     @Override
     public String toString() {
-        return "Persona{" + "cve_persona=" + cve_persona + ", nombre=" + nombre + ", ap_paterno=" + ap_paterno + ", ap_materno=" + ap_materno + ", sexo=" + sexo + ", fecha_nacimiento=" + fecha_nacimiento + ", fecha_reg=" + fecha_reg + ", fecha_mod=" + fecha_mod + ", activo=" + activo + '}';
+        return "Persona{" + "cve_persona=" + cve_persona + ", nombre=" + nombre + ", ap_paterno=" + ap_paterno + ", ap_materno=" + ap_materno + ", sexo=" + sexo + ", fecha_nacimiento=" + fecha_nacimiento + ", foto=" + foto + ", fecha_reg=" + fecha_reg + ", fecha_mod=" + fecha_mod + ", activo=" + activo + '}';
     }
 
 }
